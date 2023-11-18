553b741a0e70ca56884b405592ce5ef5b4433753
DariaYashchuk <dariayashchuk2000@gmail.com>
2023-11-18 20:57:29 +0200

    Updates

--- static/js/413.adc7b4cb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rent=self.webpackChunkcar_rent||[]).push([[413],{413:(e,c,r)=>{r.r(c),r.d(c,{default:()=>s});const s=()=>{}}}]);
-//# sourceMappingURL=413.adc7b4cb.chunk.js.map
